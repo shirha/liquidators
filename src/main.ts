293a3a1f43d5d13a6ee7a8d89741a0f47cfe360d
shirha <sh1cpan@gmail.com>
2024-08-09 18:06:34 -0700

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,10 @@ import { GUI } from 'dat.gui'
 const pts = [ // swapped z with y
   { label: "Angyuan-Raha XIX", coordinates: [0, 0, 0] },
   { label: "Iawshea-Yook", coordinates: [105, 0, 0] },
-  { label: "Nuuraya", coordinates: [63.17, 88.21, -235.86] },
-  { label: "Rudoin-Blosa", coordinates: [7.12, -176.80, -313.51] },
-  { label: "Uooyiiji", coordinates: [30.18, -5, 13.37] },
-  { label: "Taotie", coordinates: [148.96, -91.81, -156.87] }
+  { label: "Nuuraya", coordinates: [63.77, 75.57, -240.50] },
+  { label: "Rudoin-Blosa", coordinates: [7.12, -173.49, -315.36] },
+  { label: "Uooyiiji", coordinates: [30.18, -4.00, 12.73] },
+  { label: "Taotie", coordinates: [148.96, -89.93, -157.96] }
 ];
 console.log(pts);
 
